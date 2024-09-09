fc4ddeb0406aa23c08abf66de9354438ad01ae36
mateuski <mateogazzola7@gmail.com>
2024-09-09 09:09:25 -0300

    Creamos las directivas Input y Output | Conectamos la informacion transmitida entre el componente padre y el hijo | Mostramos la informacion en un carrusel y una notificacion

--- src/app/modules/producto/components/card/card.component.ts
+++ src/app/modules/producto/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { CrudService } from 'src/app/modules/admin/services/crud.service';
 
@@ -8,31 +8,39 @@ import { CrudService } from 'src/app/modules/admin/services/crud.service';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-// Definimos coleccion de productos locales
+  // Definimos colección de productos locales
   coleccionProductos: Producto[] = [];
-
-// Variable local para manejar estado de un modal
+  // Variable local para seleccionar un producto específico
   productoSeleccionado!: Producto;
-
-// Variable local para manejar estado de un modal
+  // Variable local para manejar estado de un modal
   modalVisible: boolean = false;
 
-constructor(public servicioCrud: CrudService){}
+  //Booleano para manejar visibilidad de "ultima compra"
+  compraVisible: boolean = false;
 
-ngOnInit(): void{
-  this.servicioCrud.obtenerProducto().subscribe(producto => {
-    this.coleccionProductos = producto;
-  })
-}
+  //Directivas para comunicarse con el componente padre
+  @Input() productoReciente: string = '';
 
-// Funcion para mostrar mas informacion de los productos
-  mostrarVer(Info: Producto){
+  @Output() productoAgregado = new EventEmitter<Producto>(); //@Output sera definido como un nuevo evento
 
-// Cambio estado del modal a true (ahora es visible)
-  this.modalVisible = true;
+  constructor(public servicioCrud: CrudService){}
 
-// Guardo en variable seleccionado la informacion de producto elegido
-  this.productoSeleccionado = Info
+  ngOnInit(): void{
+    this.servicioCrud.obtenerProducto().subscribe(producto => {
+      this.coleccionProductos = producto;
+    })
+  }
+  // Función para mostrar más información de los productos
+  mostrarVer(info: Producto){
+    // Cambio estado del modal a true (ahora es visible)
+    this.modalVisible = true;
+    // Guardo en variable seleccionado la información de producto elegido
+    this.productoSeleccionado = info;
+  }
 
-}
+  agregarProducto(info : Producto){
+    this.productoAgregado.emit(info);
+
+    this.compraVisible = true;
+  }
 }
--- src/app/modules/producto/producto-routing.module.ts
+++ src/app/modules/producto/producto-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     path:"juegos",component:JuegosComponent,
 
   }
-];
+];  
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
